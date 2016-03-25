612d84b25d05330a986c4044ebfa28ce10584c1b
Johannes Pfau <johannespfau@gmail.com>
2016-03-25 19:30:39 +0100

    Add tests for all accepted parameters

--- test/test.d
+++ test/test.d
@@ -183,8 +183,228 @@ void test2()
     test.files(["a"]);
 }
 
+void test3()
+{
+    auto test = GDMDTest("3");
+    environment.remove("GDC");
+    test.runSuccess("-gdc=gdc src/a.d");
+    test.files(["a"]);
+
+    // ar might not be in same folder as gdc
+    //test.runSuccess("-gdc=gdc src/a.d -lib");
+    //test.files(["a.a"]);
+
+    test.runSuccess("-gdc=gdc -ar=ar src/a.d -lib");
+    test.files(["a.a"]);
+    environment["GDC"] = "gdc";
+    environment["AR"] = "ar";
+
+    test.runSuccess("-vdmd src/a.d -lib");
+    test.files(["a.a"]);
+
+    test.runSuccess("src/a.d -q=-O3");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -O3");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -allinst");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -c");
+    test.files(["a.o"]);
+
+    test.runSuccess("src/a.d -color");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -color=on");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -color=off");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -cov");
+    test.files(["a", "a.gcno"]);
+
+    test.runSuccess("src/a.d -cov=12");
+    test.files(["a", "a.gcno"]);
+
+    test.runSuccess("src/a.d -D");
+    test.files(["a", "a.html"]);
+
+    test.runSuccess("src/a.d -D -Dddoc");
+    test.files(["a", "doc/a.html"]);
+
+    test.runSuccess("src/a.d -D -Dfdoc.html");
+    test.files(["a", "doc.html"]);
+
+    test.runSuccess("src/a.d -D -Dfdoc.html -Dddoc");
+    test.files(["a", "doc.html"]);
+
+    test.runSuccess("src/a.d -d");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -dw");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -de");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -debug=12");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -debug=test");
+    test.files(["a"]);
+
+    // Seems to be broken in GDC
+    //test.runSuccess("src/a.d -debuglib=gphobos2");
+    //test.files(["a"]);
+
+    test.runSuccess("src/a.d -defaultlib=gphobos2");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -deps");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -deps=dep.txt");
+    test.files(["a", "dep.txt"]);
+
+    test.runSuccess("src/a.d -fPIC");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -g");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -gc");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -gs");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -gx");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -H");
+    test.files(["a", "a.di"]);
+
+    test.runSuccess("src/a.d -H -Hftest.di");
+    test.files(["a", "test.di"]);
+
+    test.runSuccess("src/a.d -H -Hdimport");
+    test.files(["a", "import/a.di"]);
+
+    test.runSuccess("src/a.d --help");
+    test.files([]);
+
+    test.runSuccess("src/a.d -I.");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -ignore");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -inline");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -J.");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -lib");
+    test.files(["a.a"]);
+
+    //test.runSuccess("src/a.d -m32");
+    //test.files(["a"]);
+
+    test.runSuccess("src/a.d -m64");
+    test.files(["a"]);
+
+    test.runSuccess("src/b.d -main");
+    test.files(["b"]);
+
+    test.runSuccess("src/a.d -man");
+    test.files([]);
+
+    test.runSuccess("src/a.d -map");
+    test.files(["a", "a.map"]);
+
+    test.runSuccess("src/a.d -boundscheck=on");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -boundscheck=safeonly");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -boundscheck=off");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -noboundscheck");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -O");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -o-");
+    test.files([]);
+
+    test.runSuccess("src/a.d -profile");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -property");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -release");
+    test.files(["a"]);
+
+    test.runSuccess("-run src/a.d abcd");
+    test.files([]);
+
+    test.runFail("src/a.d -shared");
+    test.files([]);
+
+    test.runSuccess("src/a.d -transition=?");
+    test.files([]);
+
+    test.runSuccess("src/a.d -transition=tls");
+    test.files(["a"]);
+
+    test.runFail("src/a.d -transition=3449");
+    test.files([]);
+
+    test.runSuccess("src/a.d -unittest");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -v");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -vcolumns");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -version=10");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -version=foo");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -vtls");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -vgc");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -w");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -wi");
+    test.files(["a"]);
+
+    test.runSuccess("src/a.d -boundscheck=off -X");
+    test.files(["a", "a.json"]);
+
+    test.runSuccess("src/a.d -boundscheck=off -Xffoo.json");
+    test.files(["a", "foo.json"]);
+}
+
 void main()
 {
     test1();
     test2();
+    test3();
 }
